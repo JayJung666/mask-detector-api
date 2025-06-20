b3b36be65e9ed0a966a7ec77e21849a8d6a41f70
Vincent10211307 <antoniusvincent09@gmail.com>
2025-06-20 20:08:22 +0700

    ganti ke gui

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
-from ultralytics import YOLO
 import cv2
-import numpy as np
-import io
+from ultralytics import YOLO
 
-app = FastAPI()
-
-# Load model YOLO
-model = YOLO("weights/yolo11n_mask_trained.pt")
+model = YOLO("weights/best_yolo11n.pt")
 confidence_threshold = 0.5
 
-@app.get("/")
-def root():
-    return {"message": "YOLO Mask Detection API ready"}
+cap = cv2.VideoCapture(0)
 
-@app.post("/predict/")
-async def predict(file: UploadFile = File(...)):
-    contents = await file.read()
-    np_arr = np.frombuffer(contents, np.uint8)
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-    results = model.predict(img, conf=confidence_threshold, verbose=False)
+    results = model.predict(frame, conf=confidence_threshold, verbose=False)
     result = results[0]
 
     counter = {
@@ -32,9 +22,42 @@ async def predict(file: UploadFile = File(...)):
 
     if result.boxes is not None and len(result.boxes) > 0:
         for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = float(box.conf[0])
             cls = int(box.cls[0])
             class_name = model.names[cls]
+            label = f"{class_name} {conf:.2f}"
+
+            # Counter
             if class_name in counter:
                 counter[class_name] += 1
 
-    return JSONResponse(content=counter)
+            # Bounding box
+            if class_name == 'with_mask':
+                color = (0, 255, 0)
+            elif class_name == 'without_mask':
+                color = (0, 0, 255)
+            elif class_name == 'mask_weared_incorrect':
+                color = (0, 255, 255)
+            else:
+                color = (255, 255, 255)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame, label, (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+
+    # Tampilkan counter di frame
+    y_offset = 30
+    for key, value in counter.items():
+        text = f"{key}: {value}"
+        cv2.putText(frame, text, (10, y_offset),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+        y_offset += 30
+
+    cv2.imshow("Realtime Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
